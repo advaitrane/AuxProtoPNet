9e083016141fa6c9f3a9e511026381fdfb28e994
advaitrane <advaitr11@gmail.com>
2022-04-28 11:38:45 -0700

    bug fix

---
+++ saved_models_appnet/vgg19/001/settings.py
@@ -0,0 +1,48 @@
+base_architecture = 'vgg19'
+img_size = 224
+patch_size = 56
+prototype_shape = (112, 128, 1, 1)
+num_classes = 200
+prototype_activation_function = 'log'
+add_on_layers_type = 'regular'
+patch_encoder_type = 'same'
+update_grads_for_prototypes = False
+prototype_update_iter_step = 1
+
+experiment_run = '001'
+
+data_path = './datasets/cub200_cropped/'
+train_dir = data_path + 'train_cropped/'
+test_dir = data_path + 'test_cropped/'
+aux_concept_dir = data_path + 'CUB_ConceptCrops/'
+cub_data_dir = data_path + 'CUB_200_2011/'
+# train_push_dir = data_path + 'train_cropped/'
+train_batch_size = 80
+test_batch_size = 100
+# train_push_batch_size = 75
+aux_batch_size = 64
+
+joint_optimizer_lrs = {'features': 1e-4,
+                       'add_on_layers': 3e-3,
+                       # 'prototype_vectors': 3e-3
+                       }
+joint_lr_step_size = 5
+
+warm_optimizer_lrs = {'add_on_layers': 3e-3,
+                      # 'prototype_vectors': 3e-3
+                      }
+
+last_layer_optimizer_lr = 1e-4
+
+coefs = {
+    'crs_ent': 1,
+    'clst': 0.8,
+    'sep': -0.08,
+    'l1': 1e-4,
+}
+
+num_train_epochs = 50
+num_warm_epochs = 5
+
+push_start = 10
+push_epochs = [i for i in range(num_train_epochs) if i % 10 == 0]
